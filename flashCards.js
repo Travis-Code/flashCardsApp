1f36cbe07f6d2176fcbea5b504e8e2c4509cf6fc
Trav <jorkilla@gmail.com>
2016-09-17 15:15:59 -1000

    created another global variable
    refactoring to make code dry

--- flashCards.js
+++ flashCards.js
@@ -1,12 +1,11 @@
 var data_file = "http://www2.hawaii.edu/~travis27/jsonData/flashCards.json";
 //data_file gets the json file located on a specific site or directory in this case it is the flashCards.json which holds our json data that holds the answers to our questions.
+var http_request = new XMLHttpRequest();
 
 function loadJSON(){
    //loadJSON(); everything in this function will get called when a user clicks on the button on the html page.
 
-   var http_request = new XMLHttpRequest();
    http_request.onreadystatechange = function(){
-
       if (http_request.readyState == 4){
          // Javascript function JSON.parse to parse JSON data
          // refer to link for more info on ".readystate method" http://stackoverflow.com/questions/632774/what-do-the-different-readystates-in-xmlhttprequest-mean-and-how-can-i-use-them
@@ -26,18 +25,15 @@ function loadJSON(){
 }
 
 function loadJSONTwo(){
-
-   var http_request = new XMLHttpRequest();
    http_request.onreadystatechange = function(){
-
       if (http_request.readyState == 4){
          var jsonObj = JSON.parse(http_request.responseText);
          document.getElementById("flashCard2").innerHTML = "<h2>What is 2 + 1?</h2>" + jsonObj[0].flashCard2;
       }
    }
 
-   http_request.open("GET", data_file, true);
-   http_request.send();
+http_request.open("GET", data_file, true);
+http_request.send();
 }
 
 
